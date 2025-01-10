84cb2b23f8c42b3124cd51bd6d6916d1fb9b87f1
Wall_ <49759454+wall-69@users.noreply.github.com>
2025-01-10 19:23:38 +0100

    Colors, navigation, hero

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup></script>
-
 <template>
-	<main class="flex-1 flex flex-col">
-		<section>vocab learning or smth</section>
-		<section>wow image and text :O</section>
-		<section>REGISTER NOW</section>
+	<main class="flex flex-col flex-1">
+		<Navigation></Navigation>
+		<!-- HERO -->
+		<section
+			class="bg-hero h-screen-wo-nav flex flex-col items-center justify-center bg-center bg-cover">
+			<h2 class="text-primary-300 text-7xl leading-normal">DailyVocab</h2>
+			<p
+				class="max-w-[800px] max-h-[200px] text-3xl text-center text-white break-words">
+				effective vocabulary learning, focused on
+				<span class="font-medium">daily habit</span>,
+				<span class="font-medium">long&#x2011;term</span> memory storage,
+				<span class="font-medium">retrieval practice</span> and
+				<span class="font-medium">spaced repetition</span>.
+			</p>
+			<button class="btn-register-secondary mt-8">REGISTER</button>
+		</section>
+		<section>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+			voluptatem reiciendis expedita, dolores ad repudiandae suscipit iusto odit
+			sed possimus fugiat autem quos nihil? Quam porro optio repellendus totam
+			nemo? Earum id soluta impedit inventore? Fuga a sint minus similique enim
+			voluptatum doloribus repudiandae nam accusamus rerum ullam eligendi
+			maxime, quas quam quidem natus, magni consequuntur odio corrupti deleniti
+			molestias! Obcaecati cum, rerum molestiae ducimus delectus blanditiis
+			omnis pariatur rem! Reiciendis sapiente minus ipsa et accusantium neque,
+			perferendis laboriosam maiores quo quos tempore sequi ratione harum qui
+			repellat culpa odit. Possimus officia earum, dignissimos nobis
+			exercitationem alias, corporis dolore sed aperiam consequatur aliquid
+			ipsum libero adipisci maxime unde autem iure doloribus soluta optio harum
+			blanditiis qui nemo aspernatur ab! Dicta? Voluptate amet molestias
+			temporibus minima debitis numquam quisquam, nesciunt saepe, voluptatum
+			necessitatibus inventore, aperiam ipsa vitae vero libero perspiciatis
+			consectetur nobis non porro dolorem ad dolores iusto repellat? Hic,
+			commodi.
+		</section>
 	</main>
 </template>
+
+<script setup>
+import Navigation from "@/components/Navigation.vue";
+</script>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,46 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: "#F6B352",
-				secondary: "#F68657",
-				"neutral-light": "#383A3F",
-				"neutral-dark": "#1F2124",
+				"primary-100": "#B2F7FF",
+				"primary-200": "#4DEDFF",
+				"primary-300": "#38DFF5",
+				"primary-400": "#00CEE5",
+				"primary-500": "#00AABD",
+				//
+				"secondary-100": "#264AF8",
+				"secondary-200": "#0B29B8",
+				"secondary-300": "#082191",
+				"secondary-400": "#031772",
+				"secondary-500": "#08144D",
+				//
+				"red-100": "#FF7166",
+				"red-200": "#FF594D",
+				"red-300": "#F54538",
+				"red-400": "#F91706",
+				"red-500": "#C2160A",
+				//
+				"green-100": "#D0FF80",
+				"green-200": "#B3FA38",
+				"green-300": "#6FAA09",
+				"green-400": "#5D8E0B",
+				"green-500": "#2F4904",
+				//
+				"yellow-100": "#FFEF79",
+				"yellow-300": "#F5D800",
+				"yellow-500": "#C8B315",
+				//
+				"gray-100": "#F1F1F1",
+				"gray-200": "#E6E6E6",
+				"gray-300": "#CCCCCC",
+				"gray-400": "#B3B3B3",
+				"gray-500": "#999999",
+				"gray-600": "#737373",
+				"gray-700": "#353535",
+				"gray-800": "#333333",
+				"gray-900": "#1B1B1B",
+			},
+			backgroundImage: {
+				hero: "linear-gradient(0deg, rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url('@/assets/img/hero.jpg')",
 			},
 		},
 	},
