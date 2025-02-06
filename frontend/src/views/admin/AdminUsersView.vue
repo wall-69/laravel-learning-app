5b76f8e9f6fb3d8a023d5e1276a1f21fc50a8060
wall-69 <49759454+wall-69@users.noreply.github.com>
2025-02-06 18:19:22 +0100

    lil

--- frontend/src/views/admin/AdminUsersView.vue
+++ frontend/src/views/admin/AdminUsersView.vue
@@ -1,6 +1,19 @@
 <template>
-	<section class="flex flex-col flex-wrap max-w-full p-8 overflow-x-auto">
-		<h1>Users</h1>
+	<section class="lg:p-8 flex flex-col max-w-full gap-4 p-4 overflow-x-auto">
+		<h1 class="text-3xl font-bold">Users</h1>
+		<div class="lg:flex-row lg:justify-between flex flex-col items-start gap-2">
+			<button
+				@click="console.log('new')"
+				class="bg-secondary-300 hover:bg-secondary-200 px-4 py-2 font-bold text-gray-100 transition-colors rounded-md">
+				New User
+			</button>
+
+			<input
+				type="search"
+				name="filter"
+				placeholder="Search"
+				class="input bg-secondary-300 placeholder:text-gray-100 focus:placeholder:text-opacity-0 sm:block hidden text-gray-100 border-0" />
+		</div>
 
 		<DataTable :data="users"></DataTable>
 	</section>
