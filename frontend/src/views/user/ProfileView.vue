3e33aafc12df67b8c792f59a468ec85c40ea39e7
wall-69 <49759454+wall-69@users.noreply.github.com>
2025-06-09 20:16:42 +0200

    yeah

--- frontend/src/views/user/ProfileView.vue
+++ frontend/src/views/user/ProfileView.vue
@@ -4,15 +4,16 @@
 
 		<ChangeDailyReviewsForm></ChangeDailyReviewsForm>
 
-		<hr class="h-0.5 bg-gray-100 w-full" />
+		<hr class="h-0.5 bg-primary-300 w-1/3" />
 
 		<ChangePasswordForm></ChangePasswordForm>
 
-		<hr class="h-0.5 bg-gray-100 w-full" />
+		<hr class="h-0.5 bg-primary-300 w-1/3" />
 
 		<DeleteAccountForm></DeleteAccountForm>
 	</section>
 </template>
+
 <script setup>
 import ChangeDailyReviewsForm from "@/components/user/ChangeDailyReviewsForm.vue";
 import ChangePasswordForm from "@/components/user/ChangePasswordForm.vue";
